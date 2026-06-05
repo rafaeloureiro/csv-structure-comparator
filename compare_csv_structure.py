8edebd3e98fa081fbab76bcc7ad1a467e8d6da56
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 20:48:56 -0300

    Refactor compare_csv_structure function definition
    Removed docstring from compare_csv_structure function.

--- compare_csv_structure.py
+++ compare_csv_structure.py
@@ -1,25 +1,7 @@
 import pandas as pd
 
 
-def compare_csv_structure(file1: str, file2: str) -> None:
-    """
-    Compare the column structure of two CSV files.
-
-    Checks whether both files share the same set of columns.
-    If divergences are found, reports which columns are missing
-    in the new file and which ones were unexpectedly added.
-
-    Parameters
-    ----------
-    file1 : str
-        Path to the reference CSV file (expected structure).
-    file2 : str
-        Path to the received CSV file to be validated.
-
-    Returns
-    -------
-    None
-    """
+def compare_csv_structure(file1: str, file2: str):
     df1 = pd.read_csv(file1)
     df2 = pd.read_csv(file2)
 
